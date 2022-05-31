36161b6bbeea16074b08843504bc901b8ffab52c
Joey Hotz <johotz37@gmail.com>
2022-05-31 16:28:46 -0400

    Editing map of all sites
    Editing map of all sites to include POCs, and updating based on changes to other data

--- Data/Sites-Mapping.R
+++ Data/Sites-Mapping.R
@@ -23,13 +23,13 @@ IMPROVE_Sites <- read_csv("./IMPROVE Data/IMPROVE_Data_Sites_2000_2021.csv") %>%
 
 
 AQS_small <- AQS_Sites %>%
-  select(Latitude, Longitude, EPA.Code, Dataset)
+  select(Latitude, Longitude, EPA.Code, POC, Dataset)
 
 CSN_small <- CSN_Sites %>%
-  select(Latitude, Longitude, EPA.Code, Dataset)
+  select(Latitude, Longitude, EPA.Code, POC, Dataset)
 
 IMPROVE_small <- IMPROVE_Sites %>%
-  select(Latitude, Longitude, EPA.Code, Dataset)
+  select(Latitude, Longitude, EPA.Code, POC, Dataset)
 
 Sites_Info <- rbind(AQS_small, CSN_small, IMPROVE_small)
 
@@ -50,6 +50,7 @@ Sites.Map <- leaflet(data = Sites_Info) %>%
   addTiles() %>%
   addCircleMarkers(~Longitude, ~Latitude, color= ~sites_pal(Dataset), opacity = 0.5,
                    radius = 10, popup = paste("EPA Site Code:", Sites_Info$EPA.Code, "<br>",
+                                              "POC:", Sites_Info$POC, "<br>",
                                               "Dataset:", Sites_Info$Dataset)) %>%
   addLegend(position = "topright", pal = sites_pal, values = Sites_Info$Dataset,
             title = "Data Collected at Site")
