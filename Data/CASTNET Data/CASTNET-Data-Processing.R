750e39733a71912363335dcba8fc70f09344a5a2
Joey Hotz <johotz37@gmail.com>
2022-06-02 15:45:56 -0400

    Update CASTNET-Data-Processing.R

--- Data/CASTNET Data/CASTNET-Data-Processing.R
+++ Data/CASTNET Data/CASTNET-Data-Processing.R
@@ -19,9 +19,36 @@ CASTNET_sites <- read_csv("./Data/CASTNET Data/CASTNET Sites/Site.csv")
 CASTNET_full <- merge(CASTNET_raw_data, CASTNET_sites, by = c('SITE_ID')) %>%
   rename(`OBS_UPDATE_DATE` = `UPDATE_DATE.x`, `SITE_UPDATE_DATE` = `UPDATE_DATE.y`)
 
-# Save the new table as a CSV files
-write_csv(CASTNET_full, "./DATA/CASTNET Data/CASTNET_Air_Concentrations_2000_2022.csv")
+# Save the new table as a CSV file
+write_csv(CASTNET_full, "./Data/CASTNET Data/CASTNET_Air_Concentrations_2000_2022.csv")
 
-# Select all of the individual data sites which have recorded observations in our CASTNET data
-CASTNET_sites_observed <- CASTNET_sites %>% 
-  filter(SITE_ID %in% CASTNET_full$SITE_ID)
+# List all of the individual data sites for the CSN data
+CASTNET_data_sites <- CASTNET_full %>% 
+  group_by(SITE_ID, SITE_NUM, SITE_NAME, LATITUDE, LONGITUDE, STATE, COUNTY, AGENCY) %>%
+  tally()
+
+# Save the list of "used" CASTNET sites as CSV file
+write_csv(CASTNET_data_sites, "./Data/CASTNET Data/CASTNET_Data_Sites_2000_2022.csv")
+
+library(sf)
+library(leaflet)
+library(htmlwidgets)
+
+# Create an interactive map of the CASTNET data sites
+
+# Colour palette for the map
+sites_pal <- colorFactor("Set1", levels(CASTNET_data_sites$AGENCY))
+
+# Create an interactive map of CASTNET data sites
+CASTNET_sites_map <- leaflet(data = CASTNET_data_sites) %>% 
+  addTiles() %>%
+  addCircleMarkers(~LONGITUDE, ~LATITUDE, color= ~sites_pal(AGENCY), opacity = 0.5,
+                   radius = 10, popup = paste("Site Name:", CASTNET_data_sites$SITE_NAME, "<br>",
+                                              "Site ID:", CASTNET_data_sites$SITE_ID, "<br>",
+                                              "State:", CASTNET_data_sites$STATE, "<br>",
+                                              "County:", CASTNET_data_sites$COUNTY, "<br>",
+                                              "Total Observations:", CASTNET_data_sites$n)) %>%
+  addLegend(position = "topright", pal = sites_pal, values = CASTNET_data_sites$AGENCY,
+            title = "Agency operating data collection site")
+
+saveWidget(CASTNET_sites_map, file="./Data/CASTNET Data/CASTNET-Sites-Map.html")
