75656dba4a74c32973e8aa9b55df2802bab347da
Joey Hotz <johotz37@gmail.com>
2022-06-02 16:38:00 -0400

    Adding CASTNET to map of all sites

--- Data/Sites-Mapping.R
+++ Data/Sites-Mapping.R
@@ -5,36 +5,49 @@
 
 library(tidyverse)
 
-setwd("C:/Users/johot/Desktop/Joey's Files/Work/NSERC 2022/PM25-Research/Data")
+setwd("C:/Users/johot/Desktop/Joey's Files/Work/NSERC 2022/PM25-Research")
 
 # Load in CSVs containing information about sites for each dataset.
-AQS_Sites <- read_csv("./AQS Data/AQS_Data_Sites_2000_2021.csv") %>%
+# The names of these sites' attributes will be edited slightly to allow these four datasets to match one another.
+AQS_Sites <- read_csv("./Data/AQS Data/AQS_Data_Sites_2000_2021.csv") %>%
   rename(EPA.Code = Site.Code) %>%
-  mutate(Dataset = "AQS")
+  mutate(Dataset = "AQS", Site.Code = NA, Site.Name = NA)
 
-CSN_Sites <- read_csv("./CSN Data/CSN_Data_Sites_2000_2021.csv") %>%
+CSN_Sites <- read_csv("./Data/CSN Data/CSN_Data_Sites_2000_2021.csv") %>%
   rename(EPA.Code = Site.Code) %>%
-  mutate(Dataset = "CSN")
+  mutate(Dataset = "CSN", Site.Code = NA, Site.Name = NA)
 
-IMPROVE_Sites <- read_csv("./IMPROVE Data/IMPROVE_Data_Sites_2000_2021.csv") %>%
+IMPROVE_Sites <- read_csv("./Data/IMPROVE Data/IMPROVE_Data_Sites_2000_2021.csv") %>%
   select(-c("EPACode")) %>%
   mutate(EPA.Code = paste(State.Code, County.Code, Site.Num, sep = "-"),
-         Dataset = "IMPROVE")
+         Dataset = "IMPROVE") %>%
+  rename(Site.Code = SiteCode, Site.Name = SiteName)
+
+CASTNET_Sites <- read_csv("./Data/CASTNET Data/CASTNET_Data_Sites_2000_2022.csv") %>%
+  mutate(EPA.Code = NA, Dataset = "CASTNET") %>%
+  rename(Latitude = LATITUDE, Longitude = LONGITUDE, 
+         Site.Code = SITE_ID, Site.Name = SITE_NAME)
 
 
+# Collect only the attributes which we want on the map
 AQS_small <- AQS_Sites %>%
-  select(Latitude, Longitude, EPA.Code, POC, Dataset)
+  select(Latitude, Longitude, EPA.Code, Site.Code, Site.Name, Dataset)
 
 CSN_small <- CSN_Sites %>%
-  select(Latitude, Longitude, EPA.Code, POC, Dataset)
+  select(Latitude, Longitude, EPA.Code, Site.Code, Site.Name, Dataset)
 
 IMPROVE_small <- IMPROVE_Sites %>%
-  select(Latitude, Longitude, EPA.Code, POC, Dataset)
+  select(Latitude, Longitude, EPA.Code, Site.Code, Site.Name, Dataset)
 
-Sites_Info <- rbind(AQS_small, CSN_small, IMPROVE_small)
+CASTNET_small <- CASTNET_Sites %>%
+  select(Latitude, Longitude, EPA.Code, Site.Code, Site.Name, Dataset)
 
+# Merge the four different site info datasets together
+Sites_Info <- rbind(AQS_small, CSN_small, IMPROVE_small, CASTNET_small)
+
+# Remove sites with missing geographical location
 Sites_Info <- Sites_Info %>%
-  mutate(Dataset = as.factor(Dataset))
+  drop_na(Latitude, Longitude)
 
 library(sf)
 library(leaflet)
@@ -49,10 +62,11 @@ sites_pal <- colorFactor("Set1", levels(Sites_Info$Dataset))
 Sites.Map <- leaflet(data = Sites_Info) %>% 
   addTiles() %>%
   addCircleMarkers(~Longitude, ~Latitude, color= ~sites_pal(Dataset), opacity = 0.5,
-                   radius = 10, popup = paste("EPA Site Code:", Sites_Info$EPA.Code, "<br>",
-                                              "POC:", Sites_Info$POC, "<br>",
-                                              "Dataset:", Sites_Info$Dataset)) %>%
+                   radius = 7, popup = paste("Site Name:", Sites_Info$Site.Name, "<br>",
+                                             "Site Code:", Sites_Info$Site.Code, "<br>",
+                                             "EPA Site Code:", Sites_Info$EPA.Code, "<br>",
+                                             "Dataset:", Sites_Info$Dataset)) %>%
   addLegend(position = "topright", pal = sites_pal, values = Sites_Info$Dataset,
             title = "Data Collected at Site")
 
-saveWidget(Sites.Map, file="./Sites-Map.html")
+saveWidget(Sites.Map, file="./Data/Sites-Map.html")
