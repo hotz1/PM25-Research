d3fddf2c4ad4ae4cd5466a6f631721a5b8fcdf37
Joey Hotz <johotz37@gmail.com>
2022-06-20 17:13:52 -0400

    Code to download MISR data 2000-2021

--- Data/MISR/Download_MISR_NetCDFs.R
+++ Data/MISR/Download_MISR_NetCDFs.R
@@ -2,8 +2,8 @@
 # Code to download NetCDF files from NASA's MISR Level 2 Aerosol dataset
 # The files are downloaded from NASA's OPeNDAP Hyrax server (https://opendap.larc.nasa.gov/opendap/jsp/index.jsp)
 # The dataset used is the MIL2ASAE_3 dataset (https://opendap.larc.nasa.gov/opendap/hyrax/MISR/MIL2ASAE.003/contents.html)
-# The dataset contains Level 2 Aerosol parameters, with dates ranging from March 1, 2000 until November 20, 2021 (as of June 15, 2022)
-# Last updated: June 15, 2022
+# The dataset contains Level 2 Aerosol parameters, with dates ranging from March 1, 2000 until November 30, 2021 (as of June 20, 2022)
+# Last updated: June 20, 2022
 #############
 
 require(XML) 
@@ -107,31 +107,40 @@ grab.nc.url = function(url, paths = integer()){
 
 
 
-# Part 1 - Download MISR url pages from NASA site ----------------------------------------
-download.misr.urls(start = '2001-02-10', end = '2001-02-15', url.folder = 'Data/MISR/MISR_urls')
+# For loop to download and scrape html files for each year in order to collect the NetCDF file links for that year
+# This loop downloads files into a temporary file folder, reads data from these files, and deletes them after they are used.
 
-
-
-# Part 2 - Collect .nc links to download ----------------------------------
-# Collect .nc links
-# replace the file path which we call list.files on with the url folder set in Part 1 above
-misr.urls = paste0('./Data/MISR/MISR_urls/', list.files('./Data/MISR/MISR_urls'))
-
-# Scrape NetCDF file links from MISR urls and dates
-system.time({
-  ncdf.urls = unlist(sapply(misr.urls[substr(misr.urls, 23, 26) == '2001'],
-                            function(x) grab.nc.url(x, paths = c(8:55))),
+for(year in 2000:2021){
+  # Part 1 - Download MISR url pages from NASA site ----------------------------------------
+  download.misr.urls(start = paste0(year, '-01-01'), end = paste0(year, '-12-31'),
+                     url.folder = 'Data/MISR/MISR_urls/temp_files')
+  
+  # Part 2 - Collect .nc links to download ----------------------------------
+  # Collect .nc links
+  # replace the file path which we call list.files on with the url folder set in Part 1 above
+  misr.urls = paste0('./Data/MISR/MISR_urls/temp_files/', list.files('./Data/MISR/MISR_urls/temp_files'))
+  
+  # Scrape NetCDF file links from MISR urls and dates
+  system.time({
+    ncdf.urls = unlist(sapply(misr.urls[substr(misr.urls, 34, 37) == year],
+                              function(x) grab.nc.url(x, paths = c(8:55))),
                      use.names = F)
-})
-
-# optional: export vector to RDS object if not downloading all at the same time
-# saveRDS(ncdf.urls, 'ncdf_urls.rds')
+    })
+  
+  # Export vector of NetCDF links to RDS object to keep the download links for usage at a later time
+  saveRDS(ncdf.urls, paste0('./Data/MISR/MISR_urls/ncdf_urls_', year, '.rds'))
+  
+  # Find the file paths of the downloaded files and remove them from the temporary file folder
+  misr.filepaths = paste0(getwd(), '/Data/MISR/MISR_urls/temp_files')
+  misr.filepaths = paste0(misr.filepaths, '/', list.files(misr.filepaths))
+  file.remove(misr.filepaths)
+}
 
 
 
 # Part 3 - Download .nc files ---------------------------------------------
 # optional: read vector from RDS object to resume downloading
-# ncdf.urls = readRDS('ncdf_urls.rds')
+ncdf.urls = readRDS('./Data/MISR/MISR_urls/ncdf_urls_2000.rds')
 
 
 # make sure you have created a directory named ndcf in your working path (to store ncdf files)
