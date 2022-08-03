06098f8a8e7841976bef337cac1c223e4266e274
Joey Hotz <johotz37@gmail.com>
2022-08-03 12:42:39 -0400

    Update MISR_PM25_SPEC_Matching.R

--- Data/MISR/MISR_PM25_SPEC_Matching.R
+++ Data/MISR/MISR_PM25_SPEC_Matching.R
@@ -1,7 +1,7 @@
 #############
 # An R script which spatially matches previously collected and cleaned MISR Level 2 Aerosol data to
 # PM2.5 and Speciation data which were collected by the EPA within 24 hours of the MISR satellite.
-# Last updated: August 2, 2022
+# Last updated: August 3, 2022
 #############
 
 library(sf)
@@ -17,7 +17,7 @@ misr.datasets.dir = paste0(getwd(), '/Data/MISR/MISR_datasets/') # Sub-folder co
 misr.annual.filenames <- list.files(misr.datasets.dir, pattern = ".csv", full.names = T)
 misr.annual.files <- vector("list", length = length(misr.annual.filenames))
 for(i in 1:length(misr.annual.filenames)){
-  misr.annual.files[[i]] <- read_csv(misr.annual.filenames[i])
+  misr.annual.files[[i]] <- read_csv(misr.annual.filenames[i], guess_max = Inf)
 }
 misr.cali <- do.call("rbind", misr.annual.files)
 
@@ -42,4 +42,28 @@ misr.pixels <- misr.cali %>%
   ungroup()
 
 # Add pixel ID values to the main MISR dataset
-misr.cali <- merge.data.frame(misr.cali, misr.pixels)
+misr.cali <- merge.data.frame(misr.cali, misr.pixels) %>%
+  relocate(pixel_id, .before = path)
+
+
+# Create a table containing info about AQS data collection sites
+AQS.sites <- AQS.PM25.cali %>%
+  select(Longitude, Latitude, Site.Code) %>%
+  unique() %>%
+  mutate(Site.Code = paste0("AQS_", Site.Code))
+
+# Create a table containing info about CSN data collection sites
+CSN.sites <- CSN.SPEC.cali %>%
+  select(Longitude, Latitude, Site.Code) %>%
+  unique() %>%
+  mutate(Site.Code = paste0("CSN_", Site.Code))
+
+# Create a table containing info about CSN data collection sites
+IMPROVE.sites <- IMPROVE.SPEC.cali %>%
+  select(Longitude, Latitude, SiteCode) %>%
+  unique() %>%
+  mutate(Site.Code = paste0("IMPROVE_", SiteCode)) %>%
+  select(Longitude, Latitude, Site.Code)
+
+# Create a table of all data sites
+data.sites <- rbind(AQS.sites, CSN.sites, IMPROVE.sites)
