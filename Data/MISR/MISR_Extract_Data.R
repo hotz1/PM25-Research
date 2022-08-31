27010fa572593000b231d48d7b3361df7ff3cf91
Joey Hotz <johotz37@gmail.com>
2022-08-31 15:40:37 -0400

    Update MISR_Extract_Data.R
    renaming files in wget command

--- Data/MISR/MISR_Extract_Data.R
+++ Data/MISR/MISR_Extract_Data.R
@@ -227,7 +227,8 @@ for(year in start.yr:end.yr){
       # download.file(misr_urls[i], new_filename, quiet = TRUE, method = "libcurl", mode = "wb")
       
       system(paste('wget --header "Authorization: Bearer', paste0(TOKEN, '"'),
-                   '--recursive --no-parent --reject "index.html*" --execute robots=off', URL))
+                   '--recursive --no-parent --reject "index.html*" --execute robots=off', URL, 
+                   '-O', new_filename))
       
       cat("File", i, "downloaded!\n")
       
