75ae580a10ae2cef845fbc0708cf2ce78ab1cf52
Joey Hotz <johotz37@gmail.com>
2022-08-31 15:18:09 -0400

    Update MISR_Extract_Data.R
    trying wget again

--- Data/MISR/MISR_Extract_Data.R
+++ Data/MISR/MISR_Extract_Data.R
@@ -222,7 +222,7 @@ for(year in start.yr:end.yr){
       URL <- misr_urls[i]
       
       download.file(misr_urls[i], new_filename, quiet = TRUE, method = "wget", 
-                    extra = "--header \"Authorization: Bearer $TOKEN\" --recursive --no-parent --reject \"index.html*\" --execute robots=off $URL")
+                    extra = '--header "Authorization: Bearer $TOKEN" --recursive --no-parent --reject "index.html*" --execute robots=off $URL')
       cat("File", i, "downloaded!\n")
       
       cat(file.size(new_filename), "bytes.\n")
