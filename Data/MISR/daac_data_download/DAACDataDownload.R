c2550023fae31d03be5dc978be2da137b8d187c4
Joey Hotz <johotz37@gmail.com>
2022-08-31 13:24:59 -0400

    Revert "resetting git repo"
    This reverts commit 71ddbd0329659ab96ca956c529984ecbd703b99d.

--- Data/MISR/daac_data_download/DAACDataDownload.R
+++ Data/MISR/daac_data_download/DAACDataDownload.R
@@ -8,10 +8,12 @@
 # ------------------------------------------------------------------------------------------------ #
 # Check for required packages, install if not previously installed
 if ("sys" %in% rownames(installed.packages()) == FALSE) {install.packages("sys")}
+if ("getPass" %in% rownames(installed.packages()) == FALSE) { install.packages("getPass")}
 if ("httr" %in% rownames(installed.packages()) == FALSE) { install.packages("httr")}
 
 # Load necessary packages into R
 library(sys)
+library(getPass)
 library(httr)
 # ---------------------------------SET UP ENVIRONMENT--------------------------------------------- #
 
@@ -35,16 +37,15 @@ netrc <- file.path(daac.dir,'.netrc', fsep = .Platform$file.sep)  # Path to netr
 # If you already have a .netrc file with your Earthdata Login credentials stored in your home
 # directory, this portion will be skipped. Otherwise you will be prompted for your NASA Earthdata
 # Login Username/Password and a netrc file will be created to store your credentials (in home dir)
+if (file.exists(netrc) == FALSE || grepl("urs.earthdata.nasa.gov", readLines(netrc)) == FALSE) {
+  netrc_conn <- file(netrc)
 
-#if (file.exists(netrc) == FALSE || grepl("urs.earthdata.nasa.gov", readLines(netrc)) == FALSE) {
-#  netrc_conn <- file(netrc)
-#
-#  # User will be prompted for NASA Earthdata Login Username and Password below
-#  writeLines(c("machine urs.earthdata.nasa.gov",
-#               sprintf("login %s", getPass(msg = "Enter NASA Earthdata Login Username \n (or create an account at urs.earthdata.nasa.gov) :")),
-#               sprintf("password %s", getPass(msg = "Enter NASA Earthdata Login Password:"))), netrc_conn)
-#  close(netrc_conn)
-#  }
+  # User will be prompted for NASA Earthdata Login Username and Password below
+  writeLines(c("machine urs.earthdata.nasa.gov",
+               sprintf("login %s", getPass(msg = "Enter NASA Earthdata Login Username \n (or create an account at urs.earthdata.nasa.gov) :")),
+               sprintf("password %s", getPass(msg = "Enter NASA Earthdata Login Password:"))), netrc_conn)
+  close(netrc_conn)
+  }
 
 # ---------------------------CONNECT TO DATA POOL AND DOWNLOAD FILES------------------------------ #
 # Below, define either a single link to a file for download, a list of links, or a text file
@@ -71,31 +72,18 @@ year = as.integer(year)
 
 files <- readRDS(list.files(path = misr_urls.dir, pattern = paste0(year, '.rds'), full.names = T))
 
-success_counter = 0 # Count successful file downloads
-
-# Loop through all files and download them
-for (i in 1:length(files)){
-  start = Sys.time()
-  
+# Loop through all files
+for (i in 1:length(files)) {
   filename <-  tail(strsplit(files[i], '/')[[1]], n = 1) # Keep original filename
 
   # Write file to disk (authenticating with netrc) using the current directory/filename
   response <- GET(files[i], write_disk(filename, overwrite = TRUE), progress(),
                   config(netrc = TRUE, netrc_file = netrc), set_cookies("LC" = "cookies"))
 
-  download.time <- round(difftime(Sys.time(), start, units = 'secs'), 2)
-  
   # Check to see if file downloaded correctly
   if (response$status_code == 200) {
-    success_counter <- success_counter + 1
-    cat("\nFile", i, "successfully downloaded!\n")
-    cat("Time taken:", round(difftime(Sys.time(), start, units = 'secs'), 2), 'seconds\n\n')
-  } 
-  else {
-    cat("\nFile", i, "failed to download.\n\n")
+    print(sprintf("%s downloaded at %s", filename, dl_dir))
+  } else {
+  print(sprintf("%s not downloaded. Verify that your username and password are correct in %s", filename, netrc))
   }
 }
-
-cat("Year:", year, "\n")
-cat("Successful File Downloads:", success_counter, "\n")
-cat("Unsuccessful File Downloads:", files - success_counter, "\n")
