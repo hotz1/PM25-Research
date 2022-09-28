4b5c2c7962b0ffea94787ddc9353a157815f7837
Joey Hotz <johotz37@gmail.com>
2022-09-28 12:59:51 -0400

    editing mixtures correlation document and adding the heatmap

--- Data/MISR_Modelling/MISR_Mixtures_Correlation.R
+++ Data/MISR_Modelling/MISR_Mixtures_Correlation.R
@@ -1,13 +1,17 @@
 #############
 # An R script to create a "correlation heatmap" among the 74 AOD mixture models in the MISR data wihch was 
 # collected in California from 2000-2022.
-# Last updated: September 27, 2022
+# Due to space limitations, this code has been split into two sections; one which was done on the Mercury server
+# because of its higher RAM, and the other which was done locally due to a lack of permissions on said server.
+# Last updated: September 28, 2022
 #############
 
 library(tidyverse)
 library(data.table)
 
 # setwd("C:/Users/johot/Desktop/Joey's Files/Work/NSERC 2022/PM25-Research")
+
+#### Part 1: Load in MISR data, compute correlations between the values, and save it as a table
 
 # Get local file directory name
 misr.datasets.dir = paste0(getwd(), '/Data/MISR/MISR_datasets/') # Sub-folder containing yearly MISR datasets
@@ -56,24 +60,30 @@ melted_mixture.corr <- reshape2::melt(mixture.corr)
 write.csv(melted_mixture.corr, paste0(getwd(), "/Data/MISR_Modelling/Mixtures_Correlation_Melted.csv"), row.names = F)
 
 
-# # Create the correlation heatmap, and save it as an image locally.
-# mixtures_heatmap <- melted_mixture.corr %>%
-#   ggplot(aes(x = Var1, y = Var2, fill = value)) +
-#   labs(title = "Pearson Correlation between pairs of 74 AOD Mixtures from the MISR dataset",
-#        subtitle = "MISR Data collected from 2000-2022 in California") +
-#   geom_tile(color = "black") +
-#   scale_fill_gradient2(low = "#ff0000", high = "#00ff00", mid = "#ffffff",
-#                        midpoint = 0, limit = c(-1,1), name = "Correlation") +
-#   geom_text(aes(Var2, Var1, label = value), color = "black", size = 1.5) +
-#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-#         axis.title.x = element_blank(),
-#         axis.title.y = element_blank(),
-#         plot.title = element_text(hjust = 0.5, size = 25),
-#         plot.subtitle = element_text(hjust = 0.5, size = 20))
-# 
-# 
-# 
-# # Save the plot created above as a local PNG file
-# ggsave(filename = paste0(getwd(), "/Data/MISR_Modelling/Mixtures_Correlation_Heatmap.png"),
-#        plot = mixtures_heatmap, device = "png", width = 18, height = 16)
+
+##### Part 2: Load in the melted correlation table, and create a plot
+
+# Read in the csv file containing MISR data
+melted_mixture.corr <- read_csv(paste0(getwd(), "/Data/MISR_Modelling/Mixtures_Correlation_Melted.csv"))
+
+# Create the correlation heatmap, and save it as an image locally.
+mixtures_heatmap <- melted_mixture.corr %>%
+  ggplot(aes(x = Var1, y = Var2, fill = value)) +
+  labs(title = "Pearson Correlation between pairs of 74 AOD Mixtures from the MISR dataset",
+       subtitle = "MISR Data collected from 2000-2022 in California") +
+  geom_tile(color = "black") +
+  scale_fill_gradient2(low = "#ff0000", high = "#00ff00", mid = "#ffffff",
+                       midpoint = 0, limit = c(-1,1), name = "Correlation") +
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 1.5) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        plot.title = element_text(hjust = 0.5, size = 25),
+        plot.subtitle = element_text(hjust = 0.5, size = 20))
+
+
+
+# Save the plot created above as a local PNG file
+ggsave(filename = paste0(getwd(), "/Data/MISR_Modelling/Mixtures_Correlation_Heatmap.png"),
+       plot = mixtures_heatmap, device = "png", width = 18, height = 16)
 
